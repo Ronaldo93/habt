4ea525521a74da66e4279f34e59fb91d76aceb37
Ronaldo93 <51114190+Ronaldo93@users.noreply.github.com>
2026-05-06 22:37:43 +0700

    feat: init commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+
+export const Route = createFileRoute('/')({ component: Home })
+
+
+
+function Home() {
+  return (
+    <div>
+      Habt
+    </div>
+  )
+}
