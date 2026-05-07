53a84c812710355c46349cf015747535f4ac4489
Ronaldo93 <51114190+Ronaldo93@users.noreply.github.com>
2026-05-07 18:16:49 +0700

    chore: new ui design
    * feat: add new dialog creation for new habit

--- src/components/habits/CreateHabit.tsx
+++ src/components/habits/CreateHabit.tsx
@@ -1,7 +1,52 @@
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Field, FieldGroup } from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+
 export default function CreateHabit() {
     return(
-        <div>
-            {/* dialog which prompts user to */}
-        </div>
+<Dialog>
+      <form>
+        <DialogTrigger asChild>
+          <Button variant="outline">Open Dialog</Button>
+        </DialogTrigger>
+
+        {/* content */}
+        <DialogContent className="sm:max-w-sm">
+          <DialogHeader>
+            <DialogTitle>Create new habit</DialogTitle>
+            <DialogDescription>
+              Make changes to your profile here. Click save when you&apos;re
+              done.
+            </DialogDescription>
+          </DialogHeader>
+
+          {/* group */}
+          <FieldGroup>
+            <div>I want to </div> 
+            <div></div>
+          </FieldGroup>
+
+          {/* footer */}
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DialogClose>
+            <Button type="submit">Save changes</Button>
+          </DialogFooter>
+        </DialogContent>
+      </form>
+    </Dialog>
     )
 }
